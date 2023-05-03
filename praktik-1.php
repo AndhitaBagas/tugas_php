ad05914c78a670d76c06eabf5195d36d62d1cfd5
Cukiprit <bagasrisky06@gmail.com>
2023-05-03 19:33:38 +0700

    praktik 1

---
+++ praktik-1.php
@@ -0,0 +1,10 @@
+<?php
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 == 0) {
+        echo "$i bilangan ini adalah genap";
+        echo "<br>";
+    } else {
+        echo "$i bilangan ini adalah ganjil";
+        echo "<br>";
+    }
+}
